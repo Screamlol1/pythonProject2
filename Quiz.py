9392ac0c3a9597baee67c27f3c3a7ec7ceec2e41
Screamlol <k.matveev@bm-technology.ru>
2021-08-18 18:41:30 +0300

    The game is done

--- Quiz.py
+++ Quiz.py
@@ -1,25 +1,41 @@
 from tkinter import *
+from tkinter import messagebox
 
 root = Tk()
 root.title("Викторина")
-root.geometry("300x400")
+root.geometry("300x300")
 
 
 def question_one():
     question = Label(root, text="Висит груша, и её нельзя скушать")
     answer = Entry()
-    btn = Button(root, text= "Ответить")
-    question.grid(row=0)
-    answer.grid(row=1)
-    btn.grid(row=2)
+    btn = Button(root, text="Ответить", command=lambda: game1(question_two))
+    question.grid()
+    answer.grid()
+    btn.grid()
+
+    def game1(question_two):
+        if answer.get().lower() == "лампочка":
+            question_two()
+        else:
+            messagebox.showerror("Ошибка!", "Попробуй еще раз!")
+
 
 def question_two():
-    question = Label(root, text="")
-    answer = Entry()
-    btn = Button(root, text="")
+    question_2 = Label(root, text="Зимой и летом одним цветом")
+    answer_2 = Entry()
+    btn_2 = Button(root, text="Ответить", command=lambda: game2(question_two))
+    question_2.grid()
+    answer_2.grid()
+    btn_2.grid()
+
+    def game2(question_two):
+        if answer_2.get().lower() == "ёлка":
+            messagebox.showinfo("Победа", "Ты молодец!")
+        else:
+            messagebox.showerror("Ошибка", "Попрообуй еще раз!")
 
 
 question_one()
 
 root.mainloop()
-
